b1729b0a3aad53fc7459d3be2f8c1695ca0d5946
MK-SINGH-2907 <m9037400069@gmail.com>
2024-08-02 20:13:20 +0530

    Fixed errors with uploading into json file

--- options.py
+++ options.py
@@ -3,23 +3,24 @@
 import json
 
 def signup():
-    try:
-        data = json.load(open("details.json", "r+"))
-        uname = input("Enter username: ")
-        pwd = input("Enter password: ")
-        sav = float(input("Enter savings(if any): "))
-        for user in data["accounts"]:
-            if user["username"] == uname:
-                print("Username Already taken!! Use another username")
-                signup()
-            else:
-                data = data["accounts"].append({"username": uname, "password": pwd, "savings": sav, "earnings": 0, "expenditure": 0})
-    except ValueError:
-        print("Enter a number")
-        signup()
-    except Exception as e:
-        print(f"Oops! Error: {e}")
-        signup()
+    while True:
+        try:
+            data = json.load(open("details.json", "r+"))
+            uname = input("Enter username: ")
+            pwd = input("Enter password: ")
+            sav = float(input("Enter savings(if any): "))
+            for user in data["accounts"]:
+                if user["username"] == uname:
+                    print("Username Already taken!! Use another username")
+                else:
+                    data = data["accounts"].append({"username": uname, "password": pwd, "savings": sav, "earnings": 0, "expenditure": 0})
+                    with open("details.json", "w+") as f:
+                        json.dump(data, f)
+                    exit()
+        except ValueError:
+            print("Enter a number")
+        except Exception as e:
+            print(f"Oops! Error: {e}")
 
 def login():
     print("login")
