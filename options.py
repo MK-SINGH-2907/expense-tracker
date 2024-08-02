d572a335fa954e5104e915b4c89fa196bb4214ff
MK-SINGH-2907 <m9037400069@gmail.com>
2024-08-02 20:03:15 +0530

    fixed error

--- options.py
+++ options.py
@@ -7,7 +7,7 @@ def signup():
         data = json.load(open("details.json", "r+"))
         uname = input("Enter username: ")
         pwd = input("Enter password: ")
-        sav = float(input("Enter savings(if any): ")) or 0
+        sav = float(input("Enter savings(if any): "))
         for user in data["accounts"]:
             if user["username"] == uname:
                 print("Username Already taken!! Use another username")
@@ -20,6 +20,6 @@ def signup():
     except Exception as e:
         print(f"Oops! Error: {e}")
         signup()
-        
+
 def login():
     print("login")
