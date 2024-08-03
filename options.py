334ec9f63ef03fa89cb30fffb8e76f1e22f673d8
MK-SINGH-2907 <m9037400069@gmail.com>
2024-08-03 11:34:46 +0530

    Added login feature

--- options.py
+++ options.py
@@ -28,4 +28,22 @@ def signup():
             print(f"Oops! Error: {e}")
 
 def login():
-    print("login")
+    while True:
+        try:
+            data = json.load(open("details.json"))
+            uname = input("Enter username: ")
+            pwd = maskpass.askpass("Enter password: ", mask="*")
+            found = False
+            for user in data["accounts"]:
+                if user["username"] == uname and user["password"] == pwd:
+                    found = True
+                    break
+            if found:
+                print("\t Logged in!!")
+                menu(uname, pwd)
+        except Exception as e:
+            print(f"Oops! Error: {e}")
+
+def menu(uname, pwd):
+    print(uname, pwd)
+
